0c82fd12fc51f61489231663a97da9182d128ff9
takacskr <takacs.kr@gmail.com>
2024-01-05 17:57:06 +0100

    Base C# Console App.

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+            string message = "Hello World!";
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
