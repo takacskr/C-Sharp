2120601310d9bccb224b67116161e9604a0fd3ce
takacskr <takacs.kr@gmail.com>
2024-01-28 17:08:57 +0100

    Added new Apps

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+// A bináris keresőfa csomópontjait reprezentáló osztály
+class Node
+{
+    public int data; // A csomópont értéke
+    public Node left, right; // A bal és jobb gyermek csomópontok
+
+    // Konstruktor az érték beállítására
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+// A bináris keresőfa osztálya
+class BinarySearchTree
+{
+    Node root; // A fa gyökércsomópontja
+
+    // Konstruktor a gyökércsomópont inicializálására
+    public BinarySearchTree()
+    {
+        root = null;
+    }
+
+    // Beszúrási metódus a bináris keresőfába
+    void Insert(int data)
+    {
+        root = InsertRec(root, data);
+    }
+
+    // Rekurzív segédmetódus a beszúráshoz
+    Node InsertRec(Node root, int data)
+    {
+        // Ha a fa üres, létrehozunk egy új gyökércsomópontot
+        if (root == null)
+        {
+            root = new Node(data);
+            return root;
+        }
+
+        // Ellenőrizzük, hogy az új elem balra vagy jobbra kerüljön-e
+        if (data < root.data)
+            root.left = InsertRec(root.left, data);
+        else if (data > root.data)
+            root.right = InsertRec(root.right, data);
+
+        return root;
+    }
+
+    // Inorder bejárás (bal-gyökér-jobb sorrend)
+    void Inorder()
+    {
+        InorderRec(root);
+    }
+
+    // Rekurzív segédmetódus az inorder bejárás végrehajtásához
+    void InorderRec(Node root)
+    {
+        if (root != null)
+        {
+            InorderRec(root.left);
+            Console.Write(root.data + " ");
+            InorderRec(root.right);
+        }
+    }
+
+    // Main metódus a program futásának kezelésére
+    public static void Main()
+    {
+        // Példányosítjuk a bináris keresőfa objektumot
+        BinarySearchTree tree = new BinarySearchTree();
+
+        // Néhány elemet beszúrunk a fába
+        tree.Insert(50);
+        tree.Insert(30);
+        tree.Insert(20);
+        tree.Insert(40);
+        tree.Insert(70);
+        tree.Insert(60);
+        tree.Insert(80);
+
+        // Kiíratjuk az inorder bejárást, hogy ellenőrizzük a fa rendezettségét
+        Console.WriteLine("Inorder traversal of the constructed tree:");
+        tree.Inorder();
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Inheritance
+{
+    class Animal
+    {
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Nyam nyam...");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string name)
+            : base(name) { }
+
+        public override void Eat()
+        {
+            Console.WriteLine("A kutya csontot rág...");
+        }
+    }
+
+    class Crocodile : Animal
+    {
+        public Crocodile(string name)
+            : base(name) { }
+
+        public new virtual void Eat()
+        {
+            Console.WriteLine("A krokodil embert rág...");
+        }
+    }
+
+    class BigEvilCrocodile : Crocodile
+    {
+        public BigEvilCrocodile(string name)
+            : base(name) { }
+        public void Eat()
+        {
+            Console.WriteLine("A krokodil óceánjárót rág...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new("Állat");
+            Dog dog = new("Füli");
+            Crocodile crocodile = new("Aladár");
+            BigEvilCrocodile bigEvilCrocodile = new("Godzilla");
+            
+            animal.Eat();
+            dog.Eat();
+            crocodile.Eat();
+            bigEvilCrocodile.Eat();
+        }
+    }
+}
