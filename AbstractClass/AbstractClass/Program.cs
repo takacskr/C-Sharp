61a0912b01afc46fa04b384ba3723680d9b0bd68
takacskr <takacs.kr@gmail.com>
2024-01-30 19:13:25 +0100

    Added new Apps

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace AbstractClass
+{
+    abstract class Animal
+    {
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+        abstract public void Eat();
+    }
+
+    class Dog : Animal
+    {
+        public Dog(string name) : base(name) { }
+        public override void Eat()
+        {
+            Console.WriteLine("Kutya eszik...");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Egy tömböt hozunk létre, amely Animal típusú elemeket tárol
+            Animal[] animalArray = new Animal[2];
+            animalArray[0] = new Dog("Füli");
+            animalArray[1] = new Dog("Blöki");
+
+            // Hibát jelez a fodító, mert nem lehet példányosítani az absztrakt osztályt
+            // Animal animal = new Animal();
+        }
+    }
+}
---
+++ Interface2/Interface2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Interface2
+{
+    interface IAnimal
+    {
+        void Eat();
+    }
+
+    interface IDog : IAnimal
+    {
+        void Vau();
+    }
+
+    class Dog : IDog
+    {
+        public void Eat()
+        {
+            Console.WriteLine("A kutya eszik...");
+        }
+
+        public void Vau()
+        {
+            Console.WriteLine("Vau vau...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Vau();
+        }
+    }
+}
