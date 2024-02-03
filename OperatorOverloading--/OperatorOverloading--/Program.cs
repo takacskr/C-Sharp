cc1a719ba0ed925366f3c193cd2bda17cbe812e0
takacskr <takacs.kr@gmail.com>
2024-02-03 18:39:06 +0100

    Added new Apps

---
+++ OperatorOverloading--/OperatorOverloading--/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace OperatorOverloading__
+{
+    class MyInt
+    {
+        public MyInt(int value)
+        {
+            this.Value = value;
+        }
+
+        public int Value { get; private set; }
+
+        static public MyInt operator ++(MyInt rhs)
+        {
+            ++rhs.Value;
+            return rhs;
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            MyInt x = new MyInt(10);
+            Console.WriteLine(x.Value);
+            ++x;
+
+            Console.WriteLine(x.Value);
+            MyInt y = x++;
+            Console.WriteLine(x.Value);
+            Console.WriteLine(y.Value);
+        }
+    }
+}
