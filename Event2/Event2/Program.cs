4889f671ace061b2b7e69a1bb60d080690d6dcd5
takacskr <takacs.kr@gmail.com>
2024-02-09 19:48:23 +0100

    Added new Apps

---
+++ Event2/Event2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Event2
+{
+    class Test
+    {
+        public delegate void EventHandlerDelegate(string message);
+        public event EventHandlerDelegate TestStatusChange;
+        private int data = 10;
+        public int Data
+        {
+            get { return data; }
+            set
+            {
+                data = value;
+                this.OnStatusChange();
+            }
+        }
+        private void OnStatusChange()
+        {
+            if (TestStatusChange != null)
+            {
+                TestStatusChange("Az osztály állapota megváltozott!");
+            }
+        }
+    }
+    class Program
+    {
+        static public void Handler(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.TestStatusChange += Program.Handler;
+            t.Data = 11;
+            Console.ReadKey();
+        }
+    }
+}
