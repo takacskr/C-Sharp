3303829a3048aaec5e1f71e9902ce3a1a26d41c4
takacskr <takacs.kr@gmail.com>
2024-02-17 22:16:13 +0100

    Added new Apps

---
+++ GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GenericDelegate
+{
+    class Person
+    {
+        public int Age;
+    }
+
+    class Student : Person
+    {
+        public string Name { get; set; }
+    }
+
+    internal class Program
+    {
+        delegate void Method<out T>();
+
+        static void Main(string[] args)
+        {
+            Method<Student> m1 = () => new Student();
+            Method<Person> m2 = m1;
+        }
+    }
+}
---
+++ ReadOnlyCollection/ReadOnlyCollection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ReadOnlyCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>()
+                {
+                    "alma", "körte", "dió"
+                };
+
+            ReadOnlyCollection<string> roc = new ReadOnlyCollection<string>(list);
+
+            foreach (string item in roc)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
