b8b7812629e9cf50a4c039a96bcf9b1991f51d67
takacskr <takacs.kr@gmail.com>
2024-02-25 17:39:35 +0100

    Added new Apps

---
+++ Processes3/Processes3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics; // erre a névtérre szükség van
+class Program
+{
+    static void Main(string[] args)
+    {
+        Process[] processList = Process.GetProcesses(".");
+        foreach (Process process in processList)
+        {
+            Console.WriteLine("A folyamat ({0}) szálai",
+            process.ProcessName);
+            ProcessThreadCollection ptc = process.Threads;
+            foreach (ProcessThread thread in ptc)
+            {
+                Console.WriteLine("Id: {0}, Állapot: {1}",
+                thread.Id, thread.ThreadState);
+            }
+        }
+        Console.ReadKey();
+    }
+}
