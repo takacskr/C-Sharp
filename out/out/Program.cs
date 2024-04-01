8a89448822e0053962a42e8cbef25074c37f137e
takacskr <takacs.kr@gmail.com>
2024-04-01 21:14:29 +0200

    Added new Apps

---
+++ out/out/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OutKW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(out int result);
+            Console.WriteLine($"Az eredmény: {result}");
+        }
+
+        static void Calculate(out int result)
+        {
+            result = 10;
+        }
+    }
+}
