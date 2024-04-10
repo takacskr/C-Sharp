d6160ad79f3d70b797d7aad48d46fd0e4174cad0
takacskr <takacs.kr@gmail.com>
2024-04-10 20:21:17 +0200

    Added new Apps

---
+++ TestDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿class Array
+{
+    public delegate void Transformer(ref int item);
+    private int[] array;
+    public Array(int length)
+    {
+        Length = length;
+        array = new int[Length];
+    }
+    public int Length { get; set; }
+    public int this[int idx]
+    {
+        get { return array[idx]; }
+        set { array[idx] = value; }
+    }
+    public void Transform(Transformer t)
+    {
+        for (int i = 0; i < array.Length; ++i)
+        {
+            t(ref array[i]);
+        }
+    }
+}
+
+class Program
+{
+    static public void TransformerMethod(ref int item)
+    {
+        item *= item;
+    }
+    static void Main(string[] args)
+    {
+        Array array = new Array(10);
+        for (int i = 0; i < array.Length; ++i)
+        {
+            array[i] = i;
+        }
+        array.Transform(Program.TransformerMethod);
+        for (int i = 0; i < array.Length; ++i)
+        {
+            Console.WriteLine(array[i]);
+        }
+        Console.ReadKey();
+    }
+}
