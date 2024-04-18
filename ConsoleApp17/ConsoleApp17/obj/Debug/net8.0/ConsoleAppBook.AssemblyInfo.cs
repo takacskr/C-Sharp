c84f344c0de20042832558557a79e417e67e7c28
takacskr <takacs.kr@gmail.com>
2024-04-18 16:22:24 +0200

    Added new Apps

--- ConsoleApp17/ConsoleApp17/obj/Debug/net8.0/ConsoleAppBook.AssemblyInfo.cs
+++ ConsoleApp17/ConsoleApp17/obj/Debug/net8.0/ConsoleAppBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7a3c230cab03e341316e48803dd572c045ec8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0d0846a265d9814497e423fdd1a6371b2b8877")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
