5cc2035b8966d4d2d332501cd6ea74b844bbd40a
takacskr <takacs.kr@gmail.com>
2024-04-22 18:26:23 +0200

    Added new Apps

---
+++ Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello, World!";
+
+            Console.WriteLine( s1.Contains("W") );
+            Console.WriteLine( s1.EndsWith("!") );
+            Console.WriteLine( s1.IndexOf("o") );
+        }
+    }
+}
