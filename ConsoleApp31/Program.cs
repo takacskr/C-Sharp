72ddf2547285f18eb770ca362c32289f1a1b4370
takacskr <takacs.kr@gmail.com>
2024-04-23 15:04:08 +0200

    Added new Apps

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Definiálunk egy List generikus típust, amely egész számokat tárol
+        List<int> egeszLista = new List<int>();
+
+        // Hozzáadunk elemeket a listához
+        egeszLista.Add(10);
+        egeszLista.Add(20);
+
+        // Végigmegyünk a listán és kiírjuk az összes elemet
+        foreach (int elem in egeszLista)
+        {
+            Console.WriteLine(elem);
+        }
+    }
+}
