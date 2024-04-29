6646b41ac0fd2734887fa6f3fbdd87ae97eb5d24
takacskr <takacs.kr@gmail.com>
2024-04-29 19:46:26 +0200

    Added new Apps

--- yieldTest3/Program.cs
+++ yieldTest3/Program.cs
@@ -1,5 +1,15 @@
 ﻿namespace yieldTest3
 {
+    /*
+     * Ahogy látható, a GetNumbers metódus részletekben fut le, a yield return utasítások értékeket állítanak elő a sorozatban,
+     * és a Console.WriteLine utasítások futnak le közben. A yield break utasítás nem érhető el, mert a foreach ciklus abbahagyja
+     * az enumerálást a harmadik érték után.
+     * 
+     * A yield kulcsszó egy nagyon hasznos eszköz az értékek sorozatának előállítására, és sok alkalmazása van olyan területeken,
+     * mint az adatfeldolgozás, az aszinkron programozás és a kooperatív multitasking.
+     */
+
+
     internal class Program
     {
         static void Main(string[] args)
