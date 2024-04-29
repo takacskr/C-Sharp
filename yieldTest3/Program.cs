2539de90b6128cfb14e88194d20db7b3eaed1c14
takacskr <takacs.kr@gmail.com>
2024-04-29 19:43:59 +0200

    Added new Apps

---
+++ yieldTest3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace yieldTest3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> GetNumbers()
+            {
+                Console.WriteLine("Kezdés");
+                yield return 1;
+                Console.WriteLine("1 után");
+                yield return 2;
+                Console.WriteLine("2 után");
+                yield return 3;
+                Console.WriteLine("3 után");
+                yield break;
+                Console.WriteLine("Vég");
+            }
+
+            foreach (int number in GetNumbers())
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
