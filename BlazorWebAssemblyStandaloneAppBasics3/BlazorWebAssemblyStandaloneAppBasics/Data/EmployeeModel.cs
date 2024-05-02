7bb80e4f5993fd66ff6430b387548acafa7e2b3e
takacskr <takacs.kr@gmail.com>
2024-05-02 21:51:13 +0200

    Added new Apps

---
+++ BlazorWebAssemblyStandaloneAppBasics3/BlazorWebAssemblyStandaloneAppBasics/Data/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Data
+{
+    public class EmployeeModel
+    {
+        public int id { get; set; }
+        public DateTime joined { get; set; }
+        public string name { get; set; } = "";
+        public string image { get; set; } = "";
+        public string email { get; set; } = "";
+    }
+}
---
+++ BlazorWebAssemblyStandaloneAppBasics3/BlazorWebAssemblyStandaloneAppBasics/Data/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Data
+{
+    public static class EmployeeService
+    {
+        private static readonly string[] exampleNames = new[]
+        {
+            "Nancy", "Margaret", "Michael", "Robert", "Anne"
+        };
+
+        private static readonly string[] exampleMail = new[]
+        {
+            "nancy@domain.com", "margaret@domain.com", "michael@domain.com", "robert@domain.com","anne@domain.com"
+        };
+
+        public static async Task<int> TestAsync()
+        {
+            await Task.Delay(1000);
+            return 1;
+        }
+
+        public static async Task<EmployeeModel[]> GetEmployeeAsync(DateTime startDate)
+        {
+            int returnLength = 5;
+            EmployeeModel[] employeeModels = new EmployeeModel[returnLength];
+            for (int i = 0; i < returnLength; i++)
+            {
+                employeeModels[i] = new EmployeeModel
+                {
+                    joined = startDate.AddDays(i),
+                    name = exampleNames[i],
+                    email = exampleMail[i],
+                    image = "../img/" + exampleNames[i] + ".jpg",
+                    id = Random.Shared.Next(200, 555)
+                };
+            }
+
+            return employeeModels;
+
+            /*
+                return Task.FromResult(Enumerable.Range(0, 5).Select(index => new EmployeeModel
+                {
+                    joined = startDate.AddDays(index),
+                    name = exampleNames[index],
+                    email = exampleMail[index],
+                    image = "../img/" + exampleNames[index] + ".jpg",
+                    id = Random.Shared.Next(200, 555)
+                }).ToArray());
+            */
+        }
+    }
+}
