5c93993a909996ff144d9094f0840ca5ef9b55e0
takacskr <takacs.kr@gmail.com>
2024-05-07 16:32:19 +0200

    Added new Apps

--- TaskAsyncAwait-05-04/TaskAsyncAwait-05-04/Program.cs
+++ TaskAsyncAwait-05-04/TaskAsyncAwait-05-04/Program.cs
@@ -9,7 +9,7 @@ class Program
     {
         Console.WriteLine("Main task started");
 
-        SetImage("asd", null); // runs in bg, next line starts immidiately
+        SetImage("https://www.emblemazottpolo.hu/bigimage.jpg", null); // runs in bg, next line starts immidiately
 
         int x = await DoBackgroundWorkAsync(); // wait for result, set result as x
 
@@ -18,7 +18,7 @@ class Program
         for (int i = 0; i < 5; i++)
         {
             Console.WriteLine("Main task is not blocked, iteration " + i);
-            await Task.Delay(1500);
+            await Task.Delay(500);
         }
 
         Console.WriteLine("Main task completed");
@@ -27,18 +27,18 @@ class Program
 
     static async Task<int> DoBackgroundWorkAsync()
     {
-        Console.WriteLine("Background task started");
+        Console.WriteLine("Another background task started");
         await Task.Delay(5000);
-        Console.WriteLine("Background task completed");
+        Console.WriteLine("Another background task completed");
 
         return 0;
     }
 
     static async Task SetImage(string url, object targetContainerImg)
     {
-        Console.WriteLine("Background task started");
-        await Task.Delay(5000); // download image from url
-        Console.WriteLine("Background task completed");
+        Console.WriteLine("Background task started (setting image...)");
+        await Task.Delay(2000); // download image from url
+        Console.WriteLine("Background task completed (image is ready)");
 
         // targetContainerImg.image = urlResultImage.bytes;
     }
---
+++ TaskAsyncAwait-05-07/TaskAsyncAwait-05-07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+/*
+    Output:
+    Main task started
+    Another background task started
+    Main task is not blocked, iteration 0
+    Main task is not blocked, iteration 1
+    Main task is not blocked, iteration 2
+    Another background task completed
+    Main task is not blocked, iteration 3
+    Main task is not blocked, iteration 4
+    Main task completed
+ */
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Main task started");
+
+        DoBackgroundWorkAsync();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Main task is not blocked, iteration " + i);
+            await Task.Delay(1500);
+        }
+
+        Console.WriteLine("Main task completed");
+        Console.ReadLine();
+    }
+
+    static async Task<int> DoBackgroundWorkAsync()
+    {
+        Console.WriteLine("Another background task started");
+        await Task.Delay(4000);
+        Console.WriteLine("Another background task completed");
+
+        return 0;
+    }
+}
