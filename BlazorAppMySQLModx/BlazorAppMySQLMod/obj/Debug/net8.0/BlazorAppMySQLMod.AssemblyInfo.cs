b8672209883b866ecda5c542cf77b2681f1ade5f
takacskr <takacs.kr@gmail.com>
2024-05-08 09:22:43 +0200

    Added new Apps

--- BlazorAppMySQLModx/BlazorAppMySQLMod/obj/Debug/net8.0/BlazorAppMySQLMod.AssemblyInfo.cs
+++ BlazorAppMySQLModx/BlazorAppMySQLMod/obj/Debug/net8.0/BlazorAppMySQLMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppMySQLMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a2991b4267bd59201ab2ec196620c49097c10cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e551cb929fc5186efb38385b733668da72982ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppMySQLMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppMySQLMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
