00ff5b0d1395a831d8825ecee280271ad43b45ce
takacskr <takacs.kr@gmail.com>
2024-06-03 10:03:54 +0200

    Added new functions

--- Async/Async/Program.cs
+++ Async/Async/Program.cs
@@ -19,13 +19,26 @@ class Program
         return x * x;
     }
 
+    static async Task DoSomething()
+    {
+        int x = 0;
+        while (true)
+        {
+            Console.Write($"{x}");
+            x = x == 0 ? 1 : 0;
+            await Task.Delay(100);
+        }
+    }
+
     static async Task Main()
     {
         // 1. Kiírjuk, hogy a Main metódus elindult
         Console.WriteLine("Main metódus elindult a szálban...");
-       
+        
+        DoSomething();
+        
         // 2. Aszinkron módon elindítjuk a Square metódust egy háttérszálon
-        Task<int> squareTask = Task.Run( () => Square(12) );
+        // Task<int> squareTask = Task.Run( () => Square(12) );
 
         // 2.0 Várunk egy kicsit, hogy a háttérszál elinduljon és befejeződjön
         await Task.Delay(50);
@@ -33,11 +46,13 @@ class Program
         // 3. A háttérszál elindult és fut, de a Main metódus szála szabadon marad
         Console.WriteLine("Main metódus szála tovább fut, míg a háttérszál fut...");
 
+        // 3.1 Várakozunk a háttérszál befejeződésére és az eredményre
+        int result = await Square(12);
+
+        // 5. Kiírjuk az eredményt
+        Console.WriteLine($"A négyzet: {result}");
+
         // 6. Kiírjuk, hogy a Main metódus befejeződött
         Console.WriteLine("Main metódus befejeződött.");
     }
 }
-
-/*
-
- */
