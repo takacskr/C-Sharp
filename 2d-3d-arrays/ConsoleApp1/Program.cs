e77e9d551a0478f626fc820504274c0ae9c94b48
takacskr <takacs.kr@gmail.com>
2024-06-15 13:41:35 +0200

    Added a new app

--- 2d-3d-arrays/ConsoleApp1/Program.cs
+++ 2d-3d-arrays/ConsoleApp1/Program.cs
@@ -11,14 +11,29 @@
     }
 };
 
-// Think of it like a cube of integers, where each dimension has a length.
-// You can access an element in the array using three indices, like this: myArray[x, y, z]
-// Referring to the values
-Console.WriteLine(array3D[0, 0, 0]); // Outputs: 1
-Console.WriteLine(array3D[0, 0, 1]); // Outputs: 2
-Console.WriteLine(array3D[0, 1, 0]); // Outputs: 3
-Console.WriteLine(array3D[0, 1, 1]); // Outputs: 4
-Console.WriteLine(array3D[1, 0, 0]); // Outputs: 5
-Console.WriteLine(array3D[1, 0, 1]); // Outputs: 6
-Console.WriteLine(array3D[1, 1, 0]); // Outputs: 7
-Console.WriteLine(array3D[1, 1, 1]); // Outputs: 8
+for (int i = 0; i < array3D.GetLength(0); i++)
+{
+    for (int j = 0; j < array3D.GetLength(1); j++)
+    {
+        for (int k = 0; k < array3D.GetLength(2); k++)
+        {
+            System.Console.Write($"{array3D[i, j, k]} ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+/*
+    Think of it like a cube of integers, where each dimension has a length.
+    You can access an element in the array using three indices, like this: myArray[x, y, z]
+    Referring to the values
+    Console.WriteLine(array3D[0, 0, 0]); // Outputs: 1
+    Console.WriteLine(array3D[0, 0, 1]); // Outputs: 2
+    Console.WriteLine(array3D[0, 1, 0]); // Outputs: 3
+    Console.WriteLine(array3D[0, 1, 1]); // Outputs: 4
+    Console.WriteLine(array3D[1, 0, 0]); // Outputs: 5
+    Console.WriteLine(array3D[1, 0, 1]); // Outputs: 6
+    Console.WriteLine(array3D[1, 1, 0]); // Outputs: 7
+    Console.WriteLine(array3D[1, 1, 1]); // Outputs: 8
+*/
