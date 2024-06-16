ac1cda8d0373ed4120fcf7bcb99134bcd16f12dc
takacskr <takacs.kr@gmail.com>
2024-06-16 15:55:37 +0200

    Added a new app

---
+++ RekurzivFaktorialis/RekurzivFaktorialis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TestApp
+{
+    class Program
+    {
+        static public int Fact(int x)
+        {
+            if (x == 0) { return 1; }
+            else return x * Fact(x - 1);
+        }
+        static void Main(string[] args)
+        {
+            int result = Fact(10);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
