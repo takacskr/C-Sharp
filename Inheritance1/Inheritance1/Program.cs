a4008c4811e52d59912e7409b51a77ec4732f228
takacskr <takacs.kr@gmail.com>
2024-06-16 17:11:04 +0200

    Added a new app

---
+++ Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Inheritance1
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public Animal(string name)
+            {
+                this.Name = name;
+            }
+
+            public string Name { get; set; }
+
+            public virtual void Eat()
+            {
+                Console.WriteLine("Hamm hamm...");
+            }
+        }
+
+        public class Dog : Animal
+        {
+            public Dog(string name) : base(name) { }
+
+            public override void Eat()
+            {
+                Console.WriteLine("Harap harap...");
+            }
+        }
+
+        public class Crocodile : Animal
+        {
+            public Crocodile(string name) : base(name) { }
+
+            public override void Eat()
+            {
+                Console.WriteLine("Fűrészes harapás...");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog d = new Dog("Füli");
+            Crocodile c = new Crocodile("Aladár");
+
+            d.Eat();
+            c.Eat();
+        }
+    }
+}
