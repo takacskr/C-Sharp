9610e646d42ef0eb26eb1e6c41097c75444b1abc
takacskr <takacs.kr@gmail.com>
2024-06-29 16:37:09 +0200

    Added a new app

---
+++ StringReverse/StringReverse/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace StringReverse
+{
+    public static class StringExtension
+    {
+        public static string Reverse(this string str)
+        {
+            int length = str.Length;
+            char[] reverse = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                reverse[i] = str[(length -1) - i];
+            }
+
+            return new string(reverse);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "World";
+            Console.WriteLine(str.Reverse());
+        }
+    }
+}
---
+++ StringReverse2/StringReverse2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringReverse
+{
+    public static class StringExtension
+    {
+        public static string Reverse(this string str)
+        {
+            return new string(str.ToCharArray().Reverse().ToArray());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "World";
+            Console.WriteLine(str.Reverse());
+        }
+    }
+}
