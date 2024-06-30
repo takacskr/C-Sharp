7de82074c81b0f996fffe7aed9306f5c44c34940
takacskr <takacs.kr@gmail.com>
2024-06-30 14:22:21 +0200

    Added a new app

---
+++ Disemvowel0/ConsoleApp31/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp31
+{
+    public static class StrModify
+    {
+        public static string Disemvowel(this string str)
+        {
+            return String.Concat(str.Where(c => !("aeiouAEIOU".Contains(c))));
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrModify.Disemvowel("This website is for losers LOL!"));
+        }
+    }
+}
---
+++ Disemvowel1/Disemvowel/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DisemvowelString
+{
+    public static class StrMod
+    {
+        public static string Disemvowel(string str)
+        {
+            char[] disemvowels = "aeiouAEIOU".ToCharArray();
+            List<char> result = new List<char>();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (!disemvowels.Contains(str[i]))
+                {
+                    result.Add(str[i]);
+                }
+            }
+
+            return new string(result.ToArray());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrMod.Disemvowel("Hello, World!"));
+        }
+    }
+}
