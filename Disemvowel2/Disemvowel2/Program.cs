977571ba1957d2a4c9facbb69c2907f0799c4c6c
takacskr <takacs.kr@gmail.com>
2024-06-30 15:13:01 +0200

    Added a new app

---
+++ Disemvowel2/Disemvowel2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PerformanceComparison
+{
+    public static class StrMod1
+    {
+        private static readonly HashSet<char> Disemvowels = new HashSet<char>("aeiouAEIOU");
+
+        public static string Disemvowel(string str)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in str)
+            {
+                if (!Disemvowels.Contains(c))
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrMod1.Disemvowel("This website is for losers LOL!"));
+        }
+    }
+}
---
+++ Disemvowel3/Disemvowel3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace PerformanceComparison
+{
+    public static class StrMod2
+    {
+        private static readonly HashSet<char> Disemvowels = new HashSet<char>("aeiouAEIOU");
+
+        public static string Disemvowel(this string str)
+        {
+            return string.Concat(str.Where(c => !Disemvowels.Contains(c)));
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrMod2.Disemvowel("Hello World!"));
+        }
+    }
+}
